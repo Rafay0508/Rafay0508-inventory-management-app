2a254d4a73df8e1441e4e5f5850608110705fdf0
Abdul Rafay <rafayabdul508@gmail.com>
2024-10-10 01:36:55 +0500

    adding login with session and cookies

--- index.js
+++ index.js
@@ -479,7 +479,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
